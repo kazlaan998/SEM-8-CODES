8c0faba63fd39210a81220a2f1afd2db6824b7ff
Azlaan Khan <kazlaan998@gmail.com>
2026-05-12 20:12:50 +0530

    Update ass3.cpp

--- HPC/code with comments (explanations)/ass3.cpp
+++ HPC/code with comments (explanations)/ass3.cpp
@@ -1,11 +1,4 @@
 /*Implement Min, Max, Sum and Average operations using Parallel Reduction.*/
-/*
-OpenMP is an API/library used for parallel programming on CPUs i.e use multiple CPU cores at same time.
-It stands for Open Multi Processing
-You write a normal program. Then you add small OpenMP instructions called pragmas.
-Example: #pragma omp parallel
-This tells the compiler: “Run this part using multiple threads.”
-*/
 
 #include<iostream>
 #include<omp.h> //openMP library
